f417caf98949b1a570fd9572e2a2d576bc98859e
Pavnesh12 <91590106+Pavnesh12@users.noreply.github.com>
2022-03-25 09:36:32 +0530

    Add files via upload
    update is done

---
+++ SpringBoot/SpringBoot/src/main/java/com/finance/Model/FilesUploadDownload.java
@@ -0,0 +1,17 @@
+package com.finance.Model;
+
+public class FilesUploadDownload {
+    private String fileName;
+
+    public FilesUploadDownload(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
